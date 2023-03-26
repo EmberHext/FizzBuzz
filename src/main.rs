e98acc93485f0cc9ccb00a9840237f1fc3ad8379
Ember Hext <emberhext@gmail.com>
2023-03-26 14:07:58 -0400

    Fizzy

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use std::ops::Range;
+
+mod enums;
+use enums::FizzBuzzResult::FizzBuzzResult::*;
+
+fn write_range(r: Range<usize>) -> std::io::Result<()> {
+    for i in r {
+        match (i % 3, i % 5) {
+            (0, 0) => println!("{}", FizzBuzz::<String>),
+            (0, _) => println!("{}", Fizz::<String>),
+            (_, 0) => println!("{}", Buzz::<String>),
+            (_, _) => println!("{}", Number(i))
+        }
+    }
+    Ok(())
+}
+
+fn main() -> std::io::Result<()> {
+    write_range(1..101)
+}
