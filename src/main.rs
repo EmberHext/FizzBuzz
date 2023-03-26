b2b66e07db5caca297f43fd2a6f0f570285d327e
Ember Hext <emberhext@gmail.com>
2023-03-26 16:05:46 -0400

    more stuff

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,56 @@
 use std::ops::Range;
+use num_traits::PrimInt;
 
 mod enums;
-use enums::fizz_buzz_result::FizzBuzzResult::*;
+use enums::fizz_buzz_result::FizzBuzzResult::{*, self};
+
+fn fizz_type<T>(num: T) -> FizzBuzzResult<T>
+where
+T: PrimInt {
+    let zero = T::zero();
+    let three = T::from(3).expect("Failure converting 3");
+    let five = T::from(5).expect("Failure converting 5");
+    match (num % three, num % five) {
+        (a, b) if a == zero && b == zero => FizzBuzz,
+        (a, _) if a == zero => Fizz,
+        (_, a) if a == zero => Buzz,
+        (_, _) => Number(num)
+    }
+}
 
 fn write_range(r: Range<usize>) -> std::io::Result<()> {
     for i in r {
-        match (i % 3, i % 5) {
-            (0, 0) => println!("{}", FizzBuzz::<String>),
-            (0, _) => println!("{}", Fizz::<String>),
-            (_, 0) => println!("{}", Buzz::<String>),
-            (_, _) => println!("{}", Number(i))
-        }
+        println!("{}", fizz_type(i));
     }
     Ok(())
 }
 
 fn main() -> std::io::Result<()> {
     write_range(1..101)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_fizz() {
+        assert_eq!(fizz_type(3), Fizz);
+        assert_eq!(fizz_type(9), Fizz);
+        assert_eq!(fizz_type(33), Fizz);
+    }
+    
+    #[test]
+    fn test_buzz() {
+        assert_eq!(fizz_type(5), Buzz);
+        assert_eq!(fizz_type(20), Buzz);
+        assert_eq!(fizz_type(50), Buzz);
+    }
+
+    #[test]
+    fn test_fizz_buzz() {
+        assert_eq!(fizz_type(15), FizzBuzz);
+        assert_eq!(fizz_type(45), FizzBuzz);
+        assert_eq!(fizz_type(60), FizzBuzz);
+    }
 }
